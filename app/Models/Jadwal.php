01527cabacd5f6be3a7ba903f8571488881f9389
Agung Riski Ariza <agungrarz@mhs.mdp.ac.id>
2024-09-24 23:58:22 +0700

    Update guys
    update model, migrations, seeder, env

---
+++ app/Models/Jadwal.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jadwal extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+    protected $table = 'jadwal';
+
+    public function kelas(){
+        return $this->belongsTo(Kelas::class,'kelas_id','id');
+    }
+    public function matakuliah(){
+        return $this->belongsTo(MataKuliah::class,'matakuliah_id','id');
+    }
+}
