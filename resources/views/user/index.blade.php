2c50eb2e0afe5d28bf2792486e49f1151c1ae747
Tri Buana Ayu <tribuanaayu72@gmail.com>
2024-09-25 00:01:52 +0700

    User - index user
    ini adalah halaman index ges

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row">
+        <div class="col-12">
+          <div class="card mb-4">
+            <div class="card-header pb-0">
+              <h6>List User</h6>
+            </div>
+            <div class="card-body px-0 pt-0 pb-2">
+                <a href="/create-user" class="mx-2 btn btn-primary text-light">Buat User</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+@endsection
