b8e9bcda9039bf63aa648a2a673f5fc47e60a45f
Tri Buana Ayu <tribuanaayu72@gmail.com>
2024-09-25 00:08:22 +0700

    User - Mahasiswa

---
+++ resources/views/user/mahasiswa.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row">
+        <div class="col-12">
+          <div class="card mb-4">
+            <div class="card-header pb-0">
+              <h6>List Mahasiswa</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+
+@endsection
