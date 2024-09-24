9cc9cf8658f13837fef6eb7f2460aebd4251e441
Tri Buana Ayu <tribuanaayu72@gmail.com>
2024-09-24 23:55:20 +0700

    User - create user

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -4,8 +4,10 @@
 
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-
+        <h6 class="m-0 font-weight-bold text-primary">Buat User</h6>
     </div>
+    <hr>
+
 </div>
 
 @endsection
