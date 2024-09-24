28db14a1f8a4b03e7683448b1919d237fcbd0ab6
Tri Buana Ayu <tribuanaayu72@gmail.com>
2024-09-25 00:07:59 +0700

    User - Update User
    ini adalah halaman update user gess

---
+++ resources/views/user/update.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Update User</h6>
+    </div>
+    </div>
+</div>
+
+@endsection
