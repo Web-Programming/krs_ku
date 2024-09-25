3093c48ed4c289207892e08a6a91bf441b6c49c3
tribuanaayu <tribuanaayu72@gmail.com>
2024-09-25 17:37:24 +0700

    User - Mahasiswa
    ini adalah halaman mahasiswa

--- resources/views/user/mahasiswa.blade.php
+++ resources/views/user/mahasiswa.blade.php
@@ -8,6 +8,44 @@
             <div class="card-header pb-0">
               <h6>List Mahasiswa</h6>
             </div>
+            <div class="card-body px-0 pt-0 pb-2">
+              <div class="table-responsive p-0">
+                <table class="table align-items-center justify-content-center mb-0">
+                  <thead>
+                    <tr>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">No</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">NPM</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Nama</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Status</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @php
+                       $no=1; 
+                    @endphp
+                    @foreach ($user as $item)
+                      @if ($item->role != "BAA")
+                        <tr>
+                          <td>
+                            <p class="text-sm font-weight-bold mb-0">{{ $no++ }}</p>
+                          </td>
+                          <td>
+                            <h6 class="mb-0 text-sm">{{ $item->username }}</h6>
+                          </td>
+                          <td>
+                            <h6 class="mb-0 text-sm">{{ $item->nama }}</h6>
+                          </td>
+                          <td>
+                            <h6 class="mb-0 text-sm">{{ $item->status }}</h6>
+                          </td>
+                        </tr>
+                      @endif
+                    @endforeach
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
       </div>
