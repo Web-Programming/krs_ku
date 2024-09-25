59b74b84e42dee1f8173ffd3ad370d5e529812e2
tribuanaayu <tribuanaayu72@gmail.com>
2024-09-25 17:36:41 +0700

    User - Create
    ini halaman create

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -10,16 +10,26 @@
     <div class="card-body">
         <form method="POST" action="/create-user" enctype="multipart/form-data">
             @csrf
+        
             <div class="row mb-3">
                 <label for="username" class="col-md-4 col-form-label text-md-end">Username</label>
+        
+                <div class="col-md-6">
+                    <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus placeholder="Masukkan Username">
+                </div>
             </div>
+        
             <div class="row mb-3">
                 <label for="nama" class="col-md-4 col-form-label text-md-end">Nama</label>
+        
+                <div class="col-md-6">
+                    <input id="nama" type="text" class="form-control" name="nama" value="{{ old('nama') }}" required autocomplete="nama" autofocus placeholder="Masukkan Nama">
+                </div>
             </div>
 
             <div class="row mb-3">
                 <label for="role" class="col-md-4 col-form-label text-md-end">Role</label>
-
+        
                 <div class="col-md-6">
                     <select name="role" id="role" class="form-control">
                         <option value="0">--</option>
@@ -28,11 +38,15 @@
                     </select>
                 </div>
             </div>
-
+        
             <div class="row mb-3">
                 <label for="password" class="col-md-4 col-form-label text-md-end">Password</label>
+        
+                <div class="col-md-6">
+                    <input id="password" type="password" class="form-control" name="password" value="{{ old('password') }}" required autocomplete="password" autofocus placeholder="Masukkan Password">
+                </div>
             </div>
-
+                
             <div class="d-flex mb-3">
                 <div class="m-auto">
                     <button type="submit" class="btn btn-primary text-light">
@@ -42,7 +56,6 @@
             </div>
         </form>
     </div>
-
 </div>
 
 @endsection
